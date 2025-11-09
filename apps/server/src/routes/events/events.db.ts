41776d628d72ad09b166ca9f57d882b86a370a23
DominikNoga <dominik.noga2001@gmail.com>
2025-11-09 18:27:25 +0100

    fix import

--- apps/server/src/routes/events/events.db.ts
+++ apps/server/src/routes/events/events.db.ts
@@ -1,5 +1,5 @@
 import { db } from '../../db/db';
 import { TABLE_NAMES } from '../../constants/table-names';
-import { EventDbRecord } from '@gd/types';
+import { EventDbRecord } from '@gd/types/src/models/events.model';
 
 export const eventsTable = () => db<EventDbRecord>(TABLE_NAMES.EVENTS);
